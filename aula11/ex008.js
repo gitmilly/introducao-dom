f46f104367ff0bea02c8bd8958293799dda0bf37
Emilly <emillyrufino20@gmail.com>
2025-08-13 01:39:04 -0300

    dando uma estilizada

---
+++ aula11/ex008.js
@@ -0,0 +1,10 @@
+var vel = 103.5
+
+
+    console.log(`A velocidade do seu carro é ${vel}Km/h`)
+
+if(vel > 90){
+    console.log('Você está em alta velocidade: MULTADO!')
+}
+
+    console.log('Dirija sempre usando cinto de segurança!')
